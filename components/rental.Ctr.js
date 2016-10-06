62603aa91e5da42c79b7e968bd9f8271554bdfab
rajkumar <rajkumar.gootam@gmail.com>
2016-10-07 00:53:01 +0530

    created data in rental.ctr

--- components/rental.Ctr.js
+++ components/rental.Ctr.js
@@ -4,12 +4,12 @@
 angular
   .module("rentalApp")
   .controller("rentalCtrl", function($scope){
-   $scope.name = {
-     first:"raj",
-     last:"kumar"
-   };
+   $scope.rental = {
+     title:"First Item",
+     price:"$1,000,000",
+     description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+   }
 
-   $scope.message = "Hello, world!!!";
-   
+
   });
 })();
