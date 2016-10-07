a32185f90487d597d01af1492526d2b728a7c834
rajkumar <rajkumar.gootam@gmail.com>
2016-10-07 17:33:53 +0530

    Added data in rental.Ctr.js and added ng-repeat in index.html

--- components/rental.Ctr.js
+++ components/rental.Ctr.js
@@ -4,12 +4,50 @@
 angular
   .module("rentalApp")
   .controller("rentalCtrl", function($scope){
-   $scope.rental = {
-     title:"First Item",
-     price:"$1,000,000",
-     description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+   $scope.rentals = [
+   {
+     "id": 1,
+     "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
+     "title":"First Item",
+     "price":"$1,000,000",
+     "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+   },
+   {
+     "id": 2,
+     "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
+     "title":"Second Item",
+     "price":"$1,000,000",
+     "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+   },
+   {
+     "id": 3,
+     "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
+     "title":"Third Item",
+     "price":"$1,000,000",
+     "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+   },
+   {
+     "id": 4,
+     "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
+     "title":"Fourth Item",
+     "price":"$1,000,000",
+     "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+   },
+   {
+     "id": 5,
+     "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
+     "title":"Fifth Item",
+     "price":"$1,000,000",
+     "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+   },
+   {
+     "id": 6,
+     "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
+     "title":"Sixth Item",
+     "price":"$1,000,000",
+     "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    }
-
+  ]
 
   });
 })();
