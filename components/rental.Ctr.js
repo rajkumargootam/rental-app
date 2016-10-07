fcff62c574ed5a1083e8e5fc45c3e9c821a8f4bd
rajkumar <rajkumar.gootam@gmail.com>
2016-10-07 17:47:49 +0530

    Added date in rental.crt.js and viewing in index.html

--- components/rental.Ctr.js
+++ components/rental.Ctr.js
@@ -7,44 +7,50 @@ angular
    $scope.rentals = [
    {
      "id": 1,
+     "date":"2016-10-07",
      "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
      "title":"First Item",
-     "price":"$1,000,000",
+     "price":"600",
      "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    },
    {
      "id": 2,
+     "date":"2016-10-08",
      "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
      "title":"Second Item",
-     "price":"$1,000,000",
+     "price":"50",
      "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    },
    {
      "id": 3,
+     "date":"2016-10-07",
      "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
      "title":"Third Item",
-     "price":"$1,000,000",
+     "price":"30",
      "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    },
    {
      "id": 4,
+     "date":"2016-10-06",
      "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
      "title":"Fourth Item",
-     "price":"$1,000,000",
+     "price":"100",
      "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    },
    {
      "id": 5,
+     "date":"2016-10-04",
      "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
      "title":"Fifth Item",
-     "price":"$1,000,000",
+     "price":"1000",
      "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    },
    {
      "id": 6,
+     "date":"2016-10-05",
      "image":"http://www.louisianasportsman.com/classifieds/pics/p1358549934434943.jpg",
      "title":"Sixth Item",
-     "price":"$1,000,000",
+     "price":"30",
      "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
    }
   ]
