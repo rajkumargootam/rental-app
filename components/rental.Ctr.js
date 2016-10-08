00c3053af8936d80f698fd28266b34e94975da2c
rajkumar <rajkumar.gootam@gmail.com>
2016-10-08 17:08:03 +0530

    we can edit form by using admin edit button

--- components/rental.Ctr.js
+++ components/rental.Ctr.js
@@ -3,7 +3,7 @@
 
 angular
   .module("rentalApp")
-  .controller("rentalCtrl", function($scope,$http,rentalsFactory,$mdSidenav){
+  .controller("rentalCtrl", function($scope,$http,rentalsFactory,$mdSidenav,$mdToast){
 
     rentalsFactory.getrentals().then(function(rentals){
      $scope.rentals = rentals.data;
@@ -29,8 +29,20 @@ angular
       $scope.rentals.push(rental);
       $scope.rental = {};
       $scope.closeSidebar();
+      $mdToast.show(
+        $mdToast.simple()
+          .content("Rental Saved!!")
+          .position('top, right')
+          .hideDelay(3000)
+      );
       }
     }
+
+    $scope.editRental = function(rental) {
+      $scope.editing = true;
+      $scope.openSidebar();
+      $scope.rental = rental;
+     }
 
   });
 })();
