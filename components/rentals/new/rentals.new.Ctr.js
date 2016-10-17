b30f595a62b5f20b27628978c9b240abe0521227
rajkumar <rajkumar.gootam@gmail.com>
2016-10-17 15:51:13 +0530

    Added  to sidenav when the user clicks on the newrental button it wiil go to new state page and side nav will automatically generate and change the state path were to go After closing

--- components/rentals/new/rentals.new.Ctr.js
+++ components/rentals/new/rentals.new.Ctr.js
@@ -3,11 +3,25 @@
   "use strict";
   angular
     .module('rentalApp')
-    .controller('newRentalCtrl', function($mdSidenav, $mdDialog, $timeout, rentalsFactory){
+    .controller('newRentalCtrl', function($scope,$state,$mdSidenav, $mdDialog, $timeout, rentalsFactory){
       var vm = this;
+      vm.closeSidebar = closeSidebar;
+
        $timeout(function () {
         $mdSidenav('left').open();
        });
-     })
+
+       $scope.$watch('vm.sidenavOpen', function(sidenav){
+         if(sidenav === false) {
+           $mdSidenav('left').close().then(function(){
+             $state.go('rental');
+           });
+         }
+       });
+
+       function closeSidebar() {
+         vm.sidenavOpen = false;
+       }
+     });
 
    })();
